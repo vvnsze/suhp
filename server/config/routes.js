a5379a80da0a848cee9857f9f58bf2cc77023e77
Lucas Fisher <lucas.scott.fisher@gmail.com>
2016-08-27 15:55:41 -0700

    [fix] modify server to correctly route and user appropriate middleware

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,7 @@
-var controller =require('./controllers/controllers');
+var controller =require('../controllers/controllers');
 var router = require('express').Router();
 
-module.exports =function(app,express){
+
 router.post('/signup', controller.users.post);
 router.get('/signin', controller.users.get);
 
@@ -10,5 +10,7 @@ router.post('/goal', controller.goals.post);
 
 router.get('/email', controller.emails.get);
 router.post('/email', controller.emails.post);
-};
+
+
+module.exports=router;
 
--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,19 +1,21 @@
+
 var userModel = require('../models/userModel');
-var goalModel = require('../models/goalModel');
-var emailModel = require('../models/emailModel-');
+
+// var goalModel = require('/../../models/goalModel');
+// var emailModel = require('/../../models/emailModel-');
 
 
 module.exports={
 	users:{
 		get: function(req,res){
 
-			userModel.post(req.body, function(user){
-				res.send(data);
-			});
+
 
 		},
 		post:function(req,res){
-
+			userModel.post(req.body, function(user){
+				res.send(data);
+			});
 			
 		}
 
