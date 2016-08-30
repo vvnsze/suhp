36fa734decc6470d9d6f2313ce5e3a162841f662
Lee Mordell <lee.mordell@gmail.com>
2016-08-30 15:34:17 -0700

    [feature] complete email POST & GET routes

--- server/models/emailModel.js
+++ server/models/emailModel.js
@@ -1,10 +1,42 @@
 var db = require('../db/db_config.js');
 
 module.exports={
-	get:function(callback){
+	get:function(req, res){
+	
+		db.User.findOne({ where: {
+			username: req.query.username
+		}})
+		.then(function(user) {
+			db.Email.findAll({ where: { 
+				UserId: user.get('id')
+			}})
+			.then(function(emails) {
+				console.log('emails', emails);
+				res.status(200).send(emails);
+			})
+			.catch(function(err) {
+				res.status(404).send('There was an error retrieving data from the database', err);
+			})
+		});
 
 	},
-	post:function(callback){
+	post:function(req, res){
+	
+		
+		db.User.findOrCreate({where: {
+			username: req.body.username
+		}})
+		.spread(function(user, created) {
+			let emailList = req.body.emails;
+			emailList.forEach(function(address) {
+				db.Email.create({
+					UserId: user.get('id'),
+					email: address
+				}).then(function(email) {
+					res.sendStaus(201);
+				});
+			});
+		})
 
 	}
 };
