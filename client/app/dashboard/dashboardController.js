9096c56c74437ed3ffc4bff08a5ec5636d290cb7
Lauren Settle <laurjsett@gmail.com>
2016-08-30 13:05:40 -0700

    [pull] implement controller functionality

---
+++ client/app/dashboard/dashboardController.js
@@ -0,0 +1,26 @@
+angular.module('suhp.dashboard', [])
+
+.controller("DashController", function ($scope, Dashboard){
+  $scope.data = {};
+  $scope.goal = {};
+  //will render list of user goals upon initialization
+  var initializeGoals = function() {
+    Dashboard.getUserGoals()
+    .then(function(goals){
+      $scope.data.goals = goals;
+    })
+    .catch(function(error){
+      console.error(error)
+    }
+  }
+
+  initializeGoals();
+
+  //attached to ng-submit
+
+  $scope.addGoal = function(){
+    Dashboard.storeUserGoals($scope.goal)
+      //need to figure out has expired and has completed checkbox
+  }
+
+})
