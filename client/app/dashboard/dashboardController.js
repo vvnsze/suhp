349eff29aafbbf01a0360bf6c65a6eba89e30281
Lauren Settle <laurjsett@gmail.com>
2016-09-02 09:58:45 -0700

    [fix] fix merge conflict

--- client/app/dashboard/dashboardController.js
+++ client/app/dashboard/dashboardController.js
@@ -1,11 +1,7 @@
 angular.module('suhp.dashboard', [])
 
-<<<<<<< HEAD
-.controller("DashController", function (this, Dashboard){
-=======
 .controller("DashController", function (Dashboard){
-  //angular 1.5 convention. use this keyword in place of $scope 
->>>>>>> userdash
+  //angular 1.5 convention. use this keyword in place of $scope
   var vm = this;
   vm.data = {};
   vm.goal = {};
@@ -26,10 +22,8 @@ angular.module('suhp.dashboard', [])
   //attached to ng-submit
   vm.addGoal = function(){
     Dashboard.storeUserGoals(vm.goal)
-<<<<<<< HEAD
-=======
       //need to figure out has expired and has completed checkbox
->>>>>>> userdash
+
   }
 
 })
