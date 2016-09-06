bc1dca741e41d67a4d11a6b2b32819df77991eb3
Lee Mordell <lee.mordell@gmail.com>
2016-09-06 12:02:28 -0700

    [feature] clean up code and add comments in server files

--- server/config/mailgun.js
+++ server/config/mailgun.js
@@ -2,8 +2,19 @@
 The mailgun api requires a key, which was imported from './server_config.js'
 */
 
+
+/*
+Setting up mailgun
+    1) Create an account at: https://mailgun.com/sessions/new
+    2) Find your sandbox/test doman
+    3) Add authorized recipients to the account. (Mailgun's test 
+       domain will only send emails to authorized recipients)
+    4) Make sure to confirm authorization in your email
+    5) You're good to go!
+*/
 var key = require('./server_config.js');
 var domain = 'sandboxfc8ed1e2db424ce48574ca88fa53eb0e.mailgun.org';
+//Mailgun takes 2 parameters: a key and the domain address
 var mailgun = require('mailgun-js')({ apiKey: key.mg, domain: domain });
 
 
@@ -24,16 +35,16 @@ module.exports = {
             let randomIndex = Math.floor(Math.random() * gifList.length);
             //Message text & gif for email
             let message = `<div style='display:flex;'>
-      						<div style='flex-direction:row;'>
-	          					<p>Your friend 
-	          					<span style='font-weight:bold'>${req.body.username} </span>
-	          					wants you to support them in accomplishing their goal ${req.body.description}!</p>
-	          					<div style='text-align:center;'>
-	          					<img src=${gifList[randomIndex]}></img>
-	          					</div>
-      						</div>
-  						   </div>`
-            // This is the body of the email
+                            <div style='flex-direction:row;'>
+                                <p>Your friend 
+                                <span style='font-weight:bold'>${req.body.username} </span>
+                                wants you to support them in accomplishing their goal ${req.body.description}!</p>
+                                <div style='text-align:center;'>
+                                <img src=${gifList[randomIndex]}></img>
+                                </div>
+                            </div>
+                           </div>`
+                // This is the body of the email
             var data = {
                 from: 'SUHP <postmaster@sandboxfc8ed1e2db424ce48574ca88fa53eb0e.mailgun.org>',
                 to: email,
@@ -59,11 +70,11 @@ module.exports = {
             'http://media3.giphy.com/media/2jRqS4vxh2ety/200_d.gif'
         ];
 
-        userEmailList.forEach(function(email){
+        userEmailList.forEach(function(email) {
 
-        let randomIndex = Math.floor(Math.random() * gifList.length);
+            let randomIndex = Math.floor(Math.random() * gifList.length);
 
-         let message = `<div style='display:flex;'>
+            let message = `<div style='display:flex;'>
                             <div style='flex-direction:row;'>
                                 <p><span style='font-weight:bold'>${req.body.username}'s </span>
                                 goal deadline is almost here! Remind them to keep
@@ -74,20 +85,20 @@ module.exports = {
                             </div>
                            </div>`
 
-        var data = {
-            from: 'SUHP <postmaster@sandboxfc8ed1e2db424ce48574ca88fa53eb0e.mailgun.org>',
-            to: email,
-            subject: `Remind ${req.body.username} to keep working on their goal!`,
-            html: message
-        };
+            var data = {
+                from: 'SUHP <postmaster@sandboxfc8ed1e2db424ce48574ca88fa53eb0e.mailgun.org>',
+                to: email,
+                subject: `Remind ${req.body.username} to keep working on their goal!`,
+                html: message
+            };
 
-        mailgun.messages().send(data, function(error, body) {
-            console.log('body', body);
+            mailgun.messages().send(data, function(error, body) {
+                console.log('body', body);
             });
         });
     },
 
-     /*This function is wrapped in a cronjob in models/goalModel.js and sends a shame 
+    /*This function is wrapped in a cronjob in models/goalModel.js and sends a shame 
     email to the user's email list as soon as the goal deadline has passed*/
     sendShameEmails: function(userEmailList, req) {
 
@@ -97,11 +108,11 @@ module.exports = {
             'http://media3.giphy.com/media/m6ljvZNi8xnvG/200_d.gif'
         ];
 
-        userEmailList.forEach(function(email){
+        userEmailList.forEach(function(email) {
 
-        let randomIndex = Math.floor(Math.random() * gifList.length);
+            let randomIndex = Math.floor(Math.random() * gifList.length);
 
-        let message = `<div style='display:flex;'>
+            let message = `<div style='display:flex;'>
                             <div style='flex-direction:row;'>
                                 <p>Shame...<span style='font-weight:bold'>${req.body.username}</span>
                                 wasn't able to ${req.body.description}! Shame! Shame! Shame!</p>
@@ -110,17 +121,17 @@ module.exports = {
                                 </div>
                             </div>
                            </div>`
-        
 
-        var data = {
-            from: 'SUHP <postmaster@sandboxfc8ed1e2db424ce48574ca88fa53eb0e.mailgun.org>',
-            to: email,
-            subject: `Shame...${req.body.username} wasn\'t able to accomplish their goal in time!`,
-            html: message
-        };
 
-        mailgun.messages().send(data, function(error, body) {
-            console.log('body', body);
+            var data = {
+                from: 'SUHP <postmaster@sandboxfc8ed1e2db424ce48574ca88fa53eb0e.mailgun.org>',
+                to: email,
+                subject: `Shame...${req.body.username} wasn\'t able to accomplish their goal in time!`,
+                html: message
+            };
+
+            mailgun.messages().send(data, function(error, body) {
+                console.log('body', body);
             });
         });
     }
--- server/models/emailModel.js
+++ server/models/emailModel.js
@@ -1,50 +1,35 @@
 var db = require('../db/db_config.js');
-var util = require('../config/util.js');
-var mg = require('../config/mailgun.js'); //Require mailgun methods
 
-module.exports={
+module.exports = {
 
-	/*Function located in config/util.js file
-	This endpoint is not currently in use in the application
-	*/
+    /*After initial sign up, user is prompted to enter a list of friends' emails
+    This endpoint finds the user in the database, grabs his/her id, and creates
+    a list of emails in the DB associated with the userID
+    */
 
-	get:function(req, res){
-		util.getUserEmailList(req, res);
-	},
+    post: function(req, res) {
 
-	/*After initial sign up, user is prompted to enter a list of friends' emails
-	This endpoint finds the user in the database, grabs his/her id, and creates
-	a list of emails in the DB associated with the userID
-	*/
+        db.User.findOrCreate({
+                where: {
+                    username: req.body.username
+                }
+            })
+            .spread(function(user) {
+                let emailList = req.body.emails;
+                emailList.forEach(function(address) {
+                    db.Email.create({
+                        UserId: user.get('id'),
+                        email: address
+                    })
+                })
+            })
+            .then(function() {
+                //User successfully created
+                res.status(201).send('created');
 
-	post:function(req, res){
-		console.log("INSIDE EMAIL POST");
-		console.log(req.body);
-
-
-
-		db.User.findOrCreate({where: {
-			username: req.body.username
-		}})
-		.spread(function(user) {
-			let emailList = req.body.emails;
-			emailList.forEach(function(address) {
-					db.Email.create({
-						UserId: user.get('id'),
-						email: address
-						})
-					})
-		})
-		.then(function() {
-
-			//When a user is created, an initial email is sent out to user's email list
-			//mg.sendInitialEmails(req.body.emails, req, res);
-			res.status(201).send('created');
-
-		})
-		.catch(function(err) {
-		  	res.send('There was an error ', err);
-		  })
-	}
+            })
+            .catch(function(err) {
+                res.send(err);
+            })
+    }
 };
-
